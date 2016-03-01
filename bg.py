cdd31c4e2ea5782232d19d149f56188df35e6219
Daniel Jilg <daniel@elgato.com>
2016-03-01 16:56:24 +0100

    Started working on frame, cleanup

--- bg.py
+++ bg.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from PIL import Image, ImageFont, ImageDraw
 from subprocess import call
 
@@ -21,7 +22,7 @@ class ImageGenerator:
 
     @staticmethod
     def get_image():
-        return "/Users/danieljilg/Downloads/pexels-photo-48840.jpeg"
+        return "test:Boat"
 
 
 class BitterGravelist:
@@ -33,16 +34,32 @@ class BitterGravelist:
         self.image_generator = image_generator
 
     def generate_gravel(self):
-        print "gravelling..."
         image = Image.open(self.image_generator.get_image())
         draw = ImageDraw.Draw(image)
 
-        font = ImageFont.truetype("/Users/danieljilg/Downloads/master_of_break/master_of_break.ttf", 115)
-        draw.text((10, 25), "world", font=font)
+        color = '#ffc800'
+
+        # create border
+        padding = 10
+        thickness = 5
+        top = padding
+        left = padding
+        right = image.size[0] - padding
+        bottom = image.size[1] - padding
+        # turns out the method wants left, *then* top. switch.
+        draw.rectangle((top, left, bottom, left+thickness), fill=color)
+        draw.rectangle((top, left, top+thickness, right), fill=color)
+        draw.rectangle((top, right, bottom, right-thickness), fill=color)
+        draw.rectangle((bottom, left+thickness, bottom, right-thickness), fill=color)
+
+        text = self.word_generator.get_content()
+        font = ImageFont.truetype("Helvetica", 16)
+        draw.text((10, 25), text, font=font)
 
         image_save_file_name = '/Users/danieljilg//Desktop/output.png'
-        image.save(image_save_file_name, 'PNG')
-        call(['open', image_save_file_name])
+        image.show()
+        # image.save(image_save_file_name, 'PNG')
+        # call(['open', image_save_file_name])
 
 
 if __name__ == '__main__':
